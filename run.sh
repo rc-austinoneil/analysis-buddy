67ff80d2b90216eae0089aae8e20df1806675db5
Austin ONeil <austin.oneil@redcanary.com>
2023-05-07 16:01:44 -0400

    update run script to pull outside team repo

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Updating SOC Buddy"
 git pull
+echo " "
+echo "Updating team menu"
+cd team && git pull && cd ..
+echo " "
+echo "Starting SOC Buddy"
 source ./venv/bin/activate
 ./venv/bin/python3 socbuddy.py
