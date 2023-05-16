05df8ccc156bd59b30d75e045ae91a905ef13621
Austin O'Neil <austin.oneil@redcanary.com>
2023-05-16 02:20:22 -0400

    Update teammenu.py

--- team/teammenu.py
+++ team/teammenu.py
@@ -1,4 +1,4 @@
-import socbuddy
+import analysisbuddy
 import os
 from config import fontcolors, loadconfig
 from team import *
@@ -14,18 +14,18 @@ def team_name():
 # Menu
 def main_menu():
     os.system("clear")
-    socbuddy.title_bar(team_name() + " Main Menu", True)
-    socbuddy.title_bar_time()
-    socbuddy.error_message(
-        "This module allows you to use SocBuddy as a framework for your own internal tooling. To configure, please see the readme.\n"
+    analysisbuddy.title_bar(team_name() + " Main Menu", True)
+    analysisbuddy.title_bar_time()
+    analysisbuddy.error_message(
+        "This module allows you to use analysisbuddy as a framework for your own internal tooling. To configure, please see the readme.\n"
     )
-    socbuddy.menu_item(0, "Return to main menu", "goback")
+    analysisbuddy.menu_item(0, "Return to main menu", "goback")
     menu_switch(input(bcolors.INPUT + " ~> " + bcolors.ENDC))
 
 
 def menu_switch(choice):
     if choice == "0":
-        socbuddy.main_menu()
+        analysisbuddy.main_menu()
     else:
         main_menu()
 
