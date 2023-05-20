9ddf905db41b3bec442309b27f911293f9db78dc
Austin ONeil <austin.oneil@redcanary.com>
2023-05-20 02:04:07 -0400

    make setup and updating easier

--- run.sh
+++ run.sh
@@ -1,11 +1,50 @@
 #!/bin/bash
 
-echo "Updating Analysis Buddy"
-git pull
-echo " "
-echo "Updating team menu"
-cd team && git pull && cd ..
-echo " "
+read -p "Do you want to update the tool? (y/n): " answer
+
+if [[ "$answer" == [Yy]* ]]; then
+    check_requirements() {
+        local requirements_file="$1"
+        local missing_packages=()
+
+        while IFS= read -r package || [[ -n "$package" ]]; do
+            if [[ "$package" == git+* ]]; then
+                continue  # Skip Git package entries
+            fi
+
+            package_name="${package%%=*}"  # Remove version if present
+            package_name="${package_name%%<=*}"  # Remove version constraint operator if present
+
+            if ! ./venv/bin/pip freeze | grep -q "^$package_name=="; then
+                missing_packages+=("$package")
+            fi
+        done < "$requirements_file"
+
+        if [ ${#missing_packages[@]} -eq 0 ]; then
+            echo "All packages are installed."
+        else
+            echo "Installing missing packages in $requirements_file:"
+            for package in "${missing_packages[@]}"; do
+                ./venv/bin/python3 -m pip install "$package"
+            done
+        fi
+    }
+
+    echo "Updating Analysis Buddy"
+    git pull
+    source ./venv/bin/activate
+    check_requirements "./config/requirements.txt"
+
+
+    if [ -f "./team/config/requirements.txt" ]; then
+        echo " "
+        echo "Updating team menu"
+        cd team && git pull && cd ..
+        check_requirements "./team/config/requirements.txt"
+    fi
+fi
+
+# Start the tool
 echo "Starting Analysis Buddy"
 source ./venv/bin/activate
 ./venv/bin/python3 analysisbuddy.py
--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+echo " "
+echo "===== Analysis Buddy Setup ====="
 
-# Make sure you have python3-venv installed
+# Check if venv exists, if so, remove it
+if [ -d "./venv/" ]; then
+    echo "Removing existing enviroment"
+    rm -r ./venv
+fi
+
 echo "Installing dependencies"
 
 # Setup enviroment
@@ -8,6 +15,13 @@ python3 -m venv venv
 source ./venv/bin/activate
 ./venv/bin/python3 -m pip install -r ./config/requirements.txt
 
+# If your internal team has a requirements.txt file, install those too
+if [ -e "./team/config/requirements.txt" ]; then
+    echo "Installing team dependencies"
+    ./venv/bin/python3 -m pip install -r ./team/config/requirements.txt
+fi
+
+# If mac, install python-magic-bin
 if [ "$(uname)" == "Darwin" ]; then
 ./venv/bin/python3 -m pip install python-magic-bin
 fi
@@ -15,7 +29,6 @@ fi
 # Setup gitignore for teams
 git update-index --assume-unchanged team/teammenu.py
 git update-index --assume-unchanged team/config/example_config.yaml
-
 echo "team/teammenu.py" >> .git/info/exclude
 echo "team/config/example_config.yaml" >> .git/info/exclude
 
@@ -23,10 +36,24 @@ echo "team/config/example_config.yaml" >> .git/info/exclude
 echo " "
 echo "------------------------------------------"
 echo "Setup complete!"
-echo "You must configure ./config/config.yaml AND ./config/machinae.yaml before running Analysis Buddy!"
-echo " "
-echo "In machinae.yaml, you can enable tools by changing default: False to default: True"
-echo "Note: Some of the machinae sources require you to enter an API key where it says 'CHANGEME'"
-echo " "
-echo "Once the two configuration files are setup, run the tool with: ./run.sh"
-echo "------------------------------------------"
+if [ -f "./config/config.yaml" ] && [ -f "./config/machinae.yaml" ]; then
+    echo "You have existing config files, check them to make sure they are up to date!"
+    echo "New additions to the tool will be added to the example config files."
+    if [ -f "./team/config/config.yaml" ]; then
+        echo " "
+        echo "You have an existing team config file, check it to make sure its up to date!" 
+        echo "New additions to the tool will be added to the example config files."
+    fi
+    echo " "
+    echo "You can now run Analysis Buddy with: ./run.sh"
+    echo "------------------------------------------"
+else
+    echo "You must configure ./config/config.yaml AND ./config/machinae.yaml before running Analysis Buddy!"
+    echo " "
+    echo "In the machinae config, you can enable tools by changing 'default: False' to 'default: True'"
+    echo "Note: Some of the machinae sources require you to enter an API key where it says 'CHANGEME'"
+    echo " "
+    echo "Once the two configuration files are setup, run the tool with: ./run.sh"
+fi
+    echo "------------------------------------------"
+
